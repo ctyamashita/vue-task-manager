c5765a235871bedaea5e60c1a740cb8ba63c5205
celso yamashita <ctyamashita@hotmail.com>
2024-05-15 15:52:10 +0900

    wip

--- js/index.js
+++ js/index.js
@@ -11,15 +11,69 @@ createApp({
     return {
       newTitle: null,
       newDetails: null,
+      newList: null,
       tasks: oldTasks,
-      nextId: 1
+      lists: [
+        {
+          id: 1, 
+          tasks: [
+            {
+              id: 1,
+              title: 'task 1 from list 1',
+              details: 'task 1 details',
+              completed: false
+            }
+          ]
+        },
+        {
+          id: 2,
+          tasks: [
+            {
+              id: 2,
+              title: 'task 1 from list 2',
+              details: 'task 1 details',
+              completed: false
+            },
+            {
+              id: 3,
+              title: 'task 2 from list 2',
+              details: 'task 2 details',
+              completed: false
+            }
+          ]
+        }
+      ],
+      nextId: 1,
+      nextListId: 1,
+      nextTaskId: 1
     }
   },
   mounted() {
+<<<<<<< Updated upstream
     if (this.tasks.length > 0) {
       const tasks = [ ...this.tasks ]
       const lastId = tasks.sort((a, b) => a.id - b.id)
       this.nextId = lastId[this.tasks.length - 1].id + 1
+=======
+    // if (this.tasks.length > 0) {
+    //   const tasks = [ ...this.tasks ];
+    //   const lastId = tasks.sort((a, b) => a.id - b.id);
+    //   this.nextId = lastId[this.tasks.length - 1].id + 1;
+    // }
+
+    const lists = [ ...this.lists ];
+    const tasks = [];
+    lists.forEach(list=>list.tasks.forEach(task=>tasks.push(task)));
+
+    if (lists.length > 0) {
+      const lastListId = lists.sort((a,b) => a.id - b.id);
+      this.nextListId = lastListId[lists.length - 1].id + 1;
+    }
+
+    if (tasks.length > 0) {
+      const lastTaskId = tasks.sort((a,b) => a.id - b.id);
+      this.nextTaskId = lastTaskId[tasks.length - 1].id + 1; 
+>>>>>>> Stashed changes
     }
   },
   methods: {
@@ -38,6 +92,9 @@ createApp({
     deleteTask(index) {
       this.tasks.splice(index, 1)
       this.updateTasks()
+    },
+    updateLists() {
+      localStorage.setItem('lists', JSON.stringify(this.lists));
     },
     onOrderChange(event) {
       // Remove item from old index
